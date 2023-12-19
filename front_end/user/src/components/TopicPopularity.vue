f3cee2ddc40eb1856037ca939191d27b818c6ed4
wiki <12110411@mail.sustech.edu.cn>
2023-12-19 11:09:50 +0800

    front_end update topic.vue and add axios

---
+++ front_end/user/src/components/TopicPopularity.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  mounted() {
+    this.fetchTopicPopularityData();
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    fetchTopicPopularityData() {
+      axios.get('http://localhost:8080/topic-popularity/')
+          .then(response => {
+              console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
