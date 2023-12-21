3f669f275512eadc22c39dea0167fdc8038f1ff8
wiki <12110411@mail.sustech.edu.cn>
2023-12-21 21:38:08 +0800

    update topic part

--- front_end/user/src/components/BugPopularity.vue
+++ front_end/user/src/components/BugPopularity.vue
@@ -1,17 +1,35 @@
-<script setup>
-
-</script>
-
 <template>
   <div class="bug">
 
-      <div class="head">
-        <h2>
-          The visualization of bug/exception popularity
-        </h2>
-      </div>
+    <div class="head">
+      <h2>
+        The visualization of bug/exception popularity
+      </h2>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  mounted() {
+    this.fetchAllExceptionPopularity();
+  },
+
+  methods: {
+    fetchAllExceptionPopularity() {
+      axios.get('http://localhost:8090/bug-show/exception/all')
+          .then(reponse => {
+            console.log(reponse.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+  }
+}
+</script>
 
 <style scoped>
 .bug {
